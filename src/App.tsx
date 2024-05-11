f6996a48d81ef3adee57e666837d12d4564385b3
DevLogger <prin@prinkeeras.com>
2024-05-10 19:48:55 -0700

    updating example

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,9 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  // Example of using the Chrome Extension API
   const onClick = async () => {
-    let [tab] = await chrome.tabs.query({ active: true });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id! },
-      func: () => {
-        alert("Hello from the page");
-      },
-    });
+    chrome.tabs.create({ url: "https://www.google.com" });
   };
 
   return (
@@ -34,11 +29,7 @@ function App() {
       </div>
       <h1 className="text-lg font-bold">Vite + React + Tailwind CSS</h1>
       <div className="pt-5">
-        <button
-          onClick={onClick}
-        >
-          count is {0}
-        </button>
+        <button onClick={onClick}>Go to Google</button>
         <p className="pt-5">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
